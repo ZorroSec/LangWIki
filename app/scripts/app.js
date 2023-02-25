fc88ff263fc0a7a66a0e2dfb67512f9ae8c11b41
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-02-24 21:34:04 -0300

    the end?

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,13 +2,21 @@ const search = document.getElementById('search')
 const btn = document.getElementById('btn')
 const resp = document.getElementById('resp')
 
-if(btn){
-    btn.onclick = ()=>{
-        const url = `https://servicodados.ibge.gov.br/api/v3/noticias/?busca=${search.value}`
-        fetch(url).then(res =>{
-            res.json().then(data=>{
-                
-            })
-        })
-    }
-}
+const url = `https://servicodados.ibge.gov.br/api/v3/noticias/`
+fetch(url).then(res =>{
+    res.json().then(data=>{
+        for(let i=0;i<=99;i++){
+            resp.innerHTML += `
+            <div class='post'>
+                <div class='title'>
+                    <h3>${data.items[i]['titulo']}</h3>
+                </div>
+                <div class='postContent'>
+                    <p>${data.items[i]['introducao']}</p>
+                </div>
+            </div>
+            `
+            console.log(data.items[i])
+        }
+    })
+})
